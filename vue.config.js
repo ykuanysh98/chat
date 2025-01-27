809bc9b4550d1ecbac8b9642211feced5eb491a4
ykuanysh98 <ykuanysh98@gmail.com>
2025-01-27 10:49:03 +0500

    new

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
+
 module.exports = defineConfig({
-  publicPath: "/chat/",
-  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/chat/" : "/", // GitHub Pages үшін 'chat' деп сілтеу
+  outputDir: "dist", // Жиналған файлдардың қалтасы
+  assetsDir: "assets", // Байланысты файлдардың (CSS, JS) қалтасы
+  transpileDependencies: true, // Кейбір тәуелділіктерді транспиляциялау үшін
+  productionSourceMap: false, // Продакшнда source map өшіру (файлды жеңілдету үшін)
 });
